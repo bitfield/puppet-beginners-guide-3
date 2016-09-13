99d68a89ab0fdba9a1d8f542d4bf8c5376f19c80
John Arundel <john@bitfieldconsulting.com>
2016-09-13 11:55:01 +0100

    Add run-puppet script/job

---
+++ examples/run-puppet.pp
@@ -0,0 +1,10 @@
+# Set up regular Puppet runs
+file { '/usr/local/bin/run-puppet':
+  source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
+  mode   => '0755',
+}
+
+cron { '/usr/local/bin/run-puppet':
+  hour   => '*',
+  minute => '*/15',
+}
