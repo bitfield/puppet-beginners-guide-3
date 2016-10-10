d94820101e63e0d2114c5eb1e4c8ca64f9a94f4c
John Arundel <john@bitfieldconsulting.com>
2016-10-10 17:40:52 +0100

    Print out facts directly

--- examples/fact_networking.pp
+++ examples/fact_networking.pp
@@ -1,4 +1,3 @@
-$my_hostname = $facts['networking']['hostname']
-$my_fqdn = $facts['networking']['fqdn']
-$my_ip = $facts['networking']['ip']
-notice("My hostname is ${my_hostname}, my FQDN is ${my_fqdn}, and my IP is ${my_ip}")
+notice("My hostname is ${facts['networking']['hostname']}")
+notice("My FQDN is ${facts['networking']['fqdn']}")
+notice("My IP is ${facts['networking']['ip']}")
