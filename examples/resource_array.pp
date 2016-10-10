082a221620aae794fcc9c28f0fd159c7f9399104
John Arundel <john@bitfieldconsulting.com>
2016-10-10 17:41:05 +0100

    Lint

--- examples/resource_array.pp
+++ examples/resource_array.pp
@@ -1,11 +1,11 @@
 $dependencies = [ 'php7.0-cgi',
-		  'php7.0-cli',
-		  'php7.0-common',
-		  'php7.0-gd',
-		  'php7.0-json',
-		  'php7.0-mcrypt',
-		  'php7.0-mysql',
-		  'php7.0-soap']
+                  'php7.0-cli',
+                  'php7.0-common',
+                  'php7.0-gd',
+                  'php7.0-json',
+                  'php7.0-mcrypt',
+                  'php7.0-mysql',
+                  'php7.0-soap']
 
 package { $dependencies:
   ensure => installed,
