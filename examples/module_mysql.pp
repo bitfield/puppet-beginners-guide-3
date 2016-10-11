59f86a4b94f10db468ab0e57a6bbbb5d01f169d2
John Arundel <john@bitfieldconsulting.com>
2016-10-11 11:10:45 +0100

    Add mysql::db example

--- examples/module_mysql.pp
+++ examples/module_mysql.pp
@@ -3,3 +3,10 @@ class { '::mysql::server':
   root_password           => 'correct horse battery staple',
   remove_default_accounts => true,
 }
+
+mysql::db { 'cat_pictures':
+  user     => 'greebo',
+  password => 'tabby',
+  host     => 'localhost',
+  grant    => ['SELECT', 'UPDATE'],
+}
