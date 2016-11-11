cc9e18bcb483cb9db0fb65aa6372d4de7099c78b
John Arundel <john@bitfieldconsulting.com>
2016-11-11 12:20:27 +0000

    Add template examples

---
+++ examples/epp_hiera.pp
@@ -0,0 +1,4 @@
+file { '/tmp/sshd_config_example':
+  content => epp('/vagrant/examples/template_hiera_params.epp',
+    { 'users' => hiera('users') })
+}
---
+++ examples/hiera_lookup3.pp
@@ -0,0 +1,3 @@
+$cobbler_config = hiera('cobbler_config')
+$manage_dhcp = $cobbler_config['manage_dhcp']
+$pxe_just_once = $cobbler_config['pxe_just_once']
