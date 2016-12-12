4f3522223ddd4b1880778da2e8b003f4755a1261
John Arundel <john@bitfieldconsulting.com>
2016-12-12 17:20:07 +0000

    Add more Docker examples

---
+++ examples/docker_template.pp
@@ -0,0 +1,27 @@
+file { '/usr/local/bin/stop_and_remove_image':
+  source => '/vagrant/examples/stop_and_remove_image.sh',
+  mode   => '0755',
+}
+
+file { '/tmp/Dockerfile.nginx':
+  content => epp('/vagrant/examples/Dockerfile.nginx.epp',
+                  { 'message' => 'Containers rule!' }),
+  notify  => Exec['refresh-pbg-nginx'],
+}
+
+exec { 'refresh-pbg-nginx':
+  command     => '/usr/local/bin/stop_and_remove_image pbg-nginx',
+  refreshonly => true,
+  notify      => Docker::Image['pbg-nginx'],
+}
+
+docker::image { 'pbg-nginx':
+  docker_file => '/tmp/Dockerfile.nginx',
+  notify      => Docker::Run['pbg-nginx'],
+}
+
+docker::run { 'pbg-nginx':
+  image   => 'pbg-nginx',
+  ports   => ['80:80'],
+  restart => 'always',
+}
