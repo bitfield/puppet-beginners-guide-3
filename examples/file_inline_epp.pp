642bca82061b442de6f372f920e2282b06806454
John Arundel <john@bitfieldconsulting.com>
2017-02-27 15:10:40 +0000

    Define vars to placate strict-variables

--- examples/file_inline_epp.pp
+++ examples/file_inline_epp.pp
@@ -1,3 +1,6 @@
+$web_root = '/var/www/vhosts'
+$backup_dir = '/backups'
+
 file { '/usr/local/bin/backup':
   content => inline_epp('rsync -a <%= $web_root %>/ <%= $backup_dir %>/'),
   mode    => '0755',
