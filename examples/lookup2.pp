00ad571d76b250604989271110d9eb7ce3050c4f
John Arundel <john@bitfieldconsulting.com>
2017-04-28 09:50:51 +0100

    Fix quotes

--- examples/lookup2.pp
+++ examples/lookup2.pp
@@ -4,4 +4,4 @@ unless lookup('apparmor_enabled') {
   exec { 'apt-get -y remove apparmor': }
 }
 
-notice("dns_allow_query enabled: ", lookup('dns_allow_query'))
+notice('dns_allow_query enabled: ', lookup('dns_allow_query'))
--- examples/lookup3.pp
+++ examples/lookup3.pp
@@ -2,7 +2,7 @@ $cobbler_config = lookup('cobbler_config')
 $manage_dhcp = $cobbler_config['manage_dhcp']
 $pxe_just_once = $cobbler_config['pxe_just_once']
 if $pxe_just_once {
-  notice("pxe_just_once is enabled")
+  notice('pxe_just_once is enabled')
 } else {
-  notice("pxe_just_once is disabled")
+  notice('pxe_just_once is disabled')
 }
