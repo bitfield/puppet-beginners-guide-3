8e7dcd770ad08872e96fbce65880596fc85b23be
John Arundel <john@bitfieldconsulting.com>
2017-05-22 15:11:32 +0100

    Add expected type and merge strategy to lookup

--- examples/aws_hiera.pp
+++ examples/aws_hiera.pp
@@ -1,4 +1,4 @@
-$aws_resources = lookup('aws_resources')
+$aws_resources = lookup('aws_resources', Hash, 'hash')
 $aws_resources.each | String $r_type, Hash $resources | {
   $resources.each | String $r_title, Hash $attrs | {
     Resource[$r_type] { $r_title:
