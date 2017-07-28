ee884091420a8ce24c4d769867e4daaa2d8293ae
John Arundel <john@bitfieldconsulting.com>
2017-07-28 17:23:50 +0100

    Point eyaml at pbg environment

--- examples/eyaml_edit.sh
+++ examples/eyaml_edit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/puppetlabs/puppet/bin/eyaml edit --gpg-always-trust --gpg-recipients=puppet@cat-pictures.com /etc/puppetlabs/code/environments/production/hieradata/secret.eyaml
+/opt/puppetlabs/puppet/bin/eyaml edit --gpg-always-trust --gpg-recipients=puppet@cat-pictures.com /etc/puppetlabs/code/environments/pbg/data/secret.eyaml
