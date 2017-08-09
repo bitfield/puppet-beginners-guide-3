c044d3f8c3c16ec68ca567bc476de1bcec501997
John Arundel <john@bitfieldconsulting.com>
2017-08-09 13:31:23 +0100

    Don't use my real email address for cron mailto

--- examples/cron.pp
+++ examples/cron.pp
@@ -1,7 +1,7 @@
 cron { 'cron example':
   command     => '/bin/date +%F',
   user        => 'ubuntu',
-  environment => ['MAILTO=john@bitfieldconsulting.com', 'PATH=/bin'],
+  environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
   hour        => '0',
   minute      => '0',
   weekday     => ['Saturday', 'Sunday'],
