6eb1d94abfabf8932e5a248152b3ecc0c589377e
John Arundel <john@bitfieldconsulting.com>
2017-08-10 12:21:28 +0100

    Add /opt/puppetlabs/puppet/bin to secure_path

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -4,7 +4,7 @@ wget https://apt.puppetlabs.com/puppet-release-${DISTRIB_CODENAME}.deb
 dpkg -i puppet-release-${DISTRIB_CODENAME}.deb
 apt-get update
 apt-get -y install git puppet-agent
-echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin"' >/etc/sudoers.d/puppet
+echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' >/etc/sudoers.d/puppet
 /opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri
 /opt/puppetlabs/puppet/bin/gem install hiera-eyaml-gpg --no-rdoc --no-ri
 /opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri
